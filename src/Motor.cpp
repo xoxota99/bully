039c2e92a38785462e31d38213f2ff23812cd916
Phil Desrosiers <philippe.desrosiers@gmail.com>
2018-09-26 22:58:47 -0700

    Simplify motor interface.

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -7,8 +7,7 @@ Motor Motor::attach(int pwm_pin, int pin_a, int pin_b)
     m._pin_a = pin_a;
     m._pin_b = pin_b;
 
-    m.setSpeed(0);
-    m.setState(FORWARD);
+    m.move(0);
 
     return m;
 }
@@ -18,18 +17,27 @@ void Motor::detach()
     digitalWriteFast(_pwm_pin, LOW);
     digitalWriteFast(_pin_a, LOW);
     digitalWriteFast(_pin_b, LOW);
-    _motor_state = FREE;
     _attached = false;
 }
 
-motorState_t Motor::getState()
+/*
+    speed = a number between -255 and 255. The *Actual* movement occurs in the loop() function.
+*/
+void Motor::move(int speed)
 {
-    return _motor_state;
-}
-
-void Motor::setState(motorState_t state)
-{
-    _motor_state = state;
+    _motor_speed = min(255, max(-255, speed));
+    // if (_motor_speed < 0)
+    // {
+    //     _motor_state = BACKWARD;
+    // }
+    // else if (_motor_speed > 0)
+    // {
+    //     _motor_state = FORWARD;
+    // }
+    // else if (_motor_speed == 0)
+    // {
+    //     _motor_state = FREE;
+    // }
 }
 
 int Motor::getSpeed()
@@ -37,18 +45,13 @@ int Motor::getSpeed()
     return _motor_speed;
 }
 
-void Motor::setSpeed(int speed)
-{
-    _motor_speed = speed;
-}
-
 void Motor::loop()
 {
     if (_attached)
     {
-        digitalWriteFast(_pin_a, ((_motor_state == FORWARD || _motor_state == BRAKE) ? HIGH : LOW));
-        digitalWriteFast(_pin_b, ((_motor_state == BACKWARD || _motor_state == BRAKE) ? HIGH : LOW));
+        digitalWriteFast(_pin_a, (_motor_speed > 0 ? HIGH : LOW));
+        digitalWriteFast(_pin_b, (_motor_speed < 0 ? HIGH : LOW));
 
-        analogWrite(_pwm_pin, min(255, max(0, _motor_speed))); // 0 - 255
+        analogWrite(_pwm_pin, _motor_speed); // -255 to 255
     }
 }
--- src/Motor.h
+++ src/Motor.h
@@ -6,35 +6,20 @@
 #define MOTOR_H
 #include <Arduino.h>
 
-enum motorState_t
-{
-    FORWARD,
-    BACKWARD,
-    BRAKE,
-    FREE
-};
-
 class Motor
 {
   private:
     Motor(){};
 
-    int _motor_speed = 0;
-    motorState_t _motor_state = FREE;
+    int _motor_speed = 0; //between -255 and 255
     int _pwm_pin, _pin_a, _pin_b;
     bool _attached = true;
 
   public:
     static Motor attach(int pwm_pin, int pin_a, int pin_b);
     void detach();
-    // void forward(uint8_t speed);
-    // void backward(uint8_t speed);
-    // void brake();
-    motorState_t getState();
-    void setState(motorState_t state);
-
+    void move(int speed);
     int getSpeed();
-    void setSpeed(int speed);
 
     void loop();
 };
